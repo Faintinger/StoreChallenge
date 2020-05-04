a45866918c377a76270118fb410676fb5a826083
Faintinger <faintinger_dark@hotmail.com>
2020-05-04 21:39:12 +0100

    Data Layer
    Data Class implemented
    
    Load of the files into dataframes from properties location

--- Data.py
+++ Data.py
@@ -1,5 +1,6 @@
 import os;
 import datetime;
+import configparser;
 import pandas as pd;
 
 class Data(object):
@@ -12,5 +13,25 @@ class Data(object):
         Static Class
         Public Access
     """
-    
+    config = configparser.RawConfigParser();
+    config.read('config.properties');
+    loadTime=None;
+    orders=None;
+    order_lines=None;
+    products=None;
+    promotions=None;
+    product_promotions=None;
+    vendor_commissions=None;
+
+    #Load Method
+    # Load dataframes from properties file
+    @staticmethod
+    def load():
+        Data.orders = pd.read_csv(Data.config.get('DatabaseSection', 'data.order'));
+        Data.order_lines = pd.read_csv(Data.config.get('DatabaseSection', 'data.orderline'));
+        Data.products = pd.read_csv(Data.config.get('DatabaseSection', 'data.product'));
+        Data.promotions = pd.read_csv(Data.config.get('DatabaseSection', 'data.promotion'));
+        Data.product_promotions = pd.read_csv(Data.config.get('DatabaseSection', 'data.productpromotion'));
+        Data.vendor_commissions = pd.read_csv(Data.config.get('DatabaseSection', 'data.vendorcommisions'));
+        Data.loadTime=datetime.datetime.utcnow();
         
--- StoreChallenge.py
+++ StoreChallenge.py
@@ -1,6 +1,7 @@
 import flask;
 import json;
 import datetime;
+import Operations;
 import sys, traceback
 from flask import request, jsonify;
 
@@ -9,6 +10,11 @@ app = flask.Flask(__name__);
 @app.route('/HelloWorld')
 def helloworld():
     return {"Response": "Hello, World!"};
+
+
+@app.route('/DateReport', methods=['GET'])
+def report():    
+    return {'error' : 'This is not a valid date format'};
 
 
 if __name__ == '__main__':
